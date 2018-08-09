dd9a96862441c40694c5d8ba7dbfd2c4615176fb
haytam777 <haytam.elouazzani1@gmail.com>
2018-08-09 09:54:02 +0000

    add cas test

--- src/app/projet/exigence/castest-add/castest-add.component.ts
+++ src/app/projet/exigence/castest-add/castest-add.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PasDeTest } from '../../../../models/model.pasdetest';
+import { Subject, BehaviorSubject } from '../../../../../node_modules/rxjs';
 
 @Component({
   selector: 'app-castest-add',
@@ -8,10 +9,32 @@ import { PasDeTest } from '../../../../models/model.pasdetest';
 })
 export class CastestAddComponent implements OnInit {
 
-  etapes:Array<PasDeTest>;
-  constructor() { }
+  etapes:Array<PasDeTest> = new Array<PasDeTest>();
+  //etapeSubject: Subject<PasDeTest> = new BehaviorSubject<PasDeTest>(null);
+  nbrEtape:number = 0;
+
+  constructor() { 
+
+  }
 
   ngOnInit() {
+    //this.etapeSubject.subscribe(resp => this.etapes.push(resp));
   }
+
+  onAddEtapeTest() {
+    this.nbrEtape++;
+    this.etapes.push(new PasDeTest());
+  }
+
+  onMinEtapeTest() {
+
+  }
+
+  onDeleteEtapeTest(index:number) {
+    this.nbrEtape--;
+    this.etapes.splice(index,index);
+  }
+
+  
 
 }
